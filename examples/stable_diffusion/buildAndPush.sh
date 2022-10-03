2a16e3d58448dbeb0d1ced23a2207856a39b219b
SergioLangaritaBenitez <sergiolangaritabenitez@gmail.com>
2022-10-03 11:49:26 +0200

    stable diffusion name of image

--- examples/stable_diffusion/buildAndPush.sh
+++ examples/stable_diffusion/buildAndPush.sh
@@ -1,2 +1,2 @@
-docker build -t ghcr.io/grycap/stable_diffusion .
-docker push  ghcr.io/grycap/stable_diffusion
+docker build -t ghcr.io/grycap/gradio_stable_diffusion .
+docker push  ghcr.io/grycap/gradio_stable_diffusion
